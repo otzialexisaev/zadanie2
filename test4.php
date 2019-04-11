85e35937755aa7519dc120502b8034cc1396d257
otzialexisaev <runyanavenue98@gmail.com>
2019-04-11 16:20:49 +0300

    PDO

--- test4.php
+++ test4.php
@@ -8,19 +8,19 @@ $conn = new MysQLDB();
 $conn->connect('localhost','root','','test4');
 
 $arrayAll = $conn->getTableContentAsArrayAll('configurations');
-print_r($arrayAll);
+//print_r($arrayAll);
 echo "<br>";
 for($i = 0; $i<sizeof($arrayAll); $i++){
   echo $arrayAll[$i]['config']."<br>";
 }
 
 $arrayOne = $conn->getTableContentAsArrayOne('configurations', 1);
-print_r($arrayOne);
+//print_r($arrayOne);
 echo "<br>";
 echo "id: ".$arrayOne['id'] . "<br>Config: " . $arrayOne['config'] . "<br>";
 
 $arrayOne = $conn->getTableContentAsArrayOne('configurations', 2);
-print_r($arrayOne);
+//print_r($arrayOne);
 echo "<br>";
 echo "id: ".$arrayOne['id'] . "<br>Config: " . $arrayOne['config'] . "<br>";
 echo "<hr>";
@@ -29,14 +29,19 @@ echo "<hr>";
 $conn = new PDODB();
 $conn->connect('localhost','root','','test4');
 $arrayAll = $conn->getTableContentAsArrayAll('configurations');
-print_r($arrayAll);
+//print_r($arrayAll);
 echo "<br>";
 for($i = 0; $i<sizeof($arrayAll); $i++){
   echo $arrayAll[$i]['config']."<br>";
 }
 
 $arrayOne = $conn->getTableContentAsArrayOne('configurations', 1);
-print_r($arrayOne);
+//print_r($arrayOne);
+echo "<br>";
+echo "id: ".$arrayOne['id'] . "<br>Config: " . $arrayOne['config'] . "<br>";
+
+$arrayOne = $conn->getTableContentAsArrayOne('configurations', 2);
+//print_r($arrayOne);
 echo "<br>";
 echo "id: ".$arrayOne['id'] . "<br>Config: " . $arrayOne['config'] . "<br>";
 
